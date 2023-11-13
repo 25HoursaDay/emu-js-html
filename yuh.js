be7233b71f9deadf5bcd8ba581b25dc5db5e960d
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-11-13 10:00:50 -0600

    Update yuh.js

--- yuh.js
+++ yuh.js
@@ -1,7 +1,5 @@
   var params = new URLSearchParams(
-    function() {
-      (document.currentScript.src !== (undefined || null || "")) ? document.currentScript.src.search: document.location.search;
-    }
+    (document.currentScript.src !== (undefined || null || "")) ? document.currentScript.src.search : document.location.search
   );
   var value = params.get("sf");
   console.log(value);
