6ad58c07b22e29fc31f5ca31c6c9323ee64085ba
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-11-16 09:26:52 -0600

    Delete yuh.js

--- yuh.js
+++
@@ -1,5 +0,0 @@
-  var params = new URLSearchParams(
-    (document.currentScript.src !== (undefined || null || "")) ? document.currentScript.src.search : document.location.search
-  );
-  var value = params.get("sf");
-  console.log(value);
